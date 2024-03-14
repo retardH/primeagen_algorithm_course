b7479fcd52be3d3de10ca3d3176aa405dbd50a1c
HtetZarni <loowmaad@gmail.com>
2024-03-14 16:56:45 +0630

    init

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,16 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (value < needle) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    } while (low < high);
+    return false;
+}
