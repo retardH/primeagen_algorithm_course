bafa451354781cd8e54c2cba55bd2ccd6b4aad4b
HtetZarni <loowmaad@gmail.com>
2024-03-15 00:03:02 +0630

    implement queue and stack

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,17 +1,96 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class SinglyLinkedList<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
 
-    prepend(item: T): void {}
+    prepend(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        if (!this.head) {
+            this.head = this.tail = newNode;
+            this.length = 1;
+            return;
+        }
 
-    insertAt(item: T, idx: number): void {}
+        this.length++;
+        this.head = newNode;
+    }
 
-    append(item: T): void {}
+    append(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+            this.length = 1;
+            return;
+        }
+        this.length++;
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
 
-    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {
+        let currentNode = this.head;
+        if (idx > this.length - 1 && !currentNode) {
+            return undefined;
+        }
 
-    get(idx: number): T | undefined {}
+        for (let i = 0; i < this.length; i++) {
+            if (idx === i) {
+                break;
+            }
+            currentNode = currentNode?.next;
+        }
 
-    removeAt(idx: number): T | undefined {}
+        return currentNode?.value;
+    }
+
+    insertAt(item: T, idx: number): void {
+        let node = { value: item } as Node<T>;
+        let currentNode = this.head;
+        if (idx >= this.length) {
+            return;
+        }
+
+        for (let i = 0; i < this.length; i++) {
+            if (idx === i) {
+                break;
+            }
+            currentNode = currentNode?.next;
+        }
+
+        node.next = currentNode;
+        currentNode = node;
+    }
+
+    remove(item: T): T | undefined {
+        return undefined;
+    }
+
+    removeAt(idx: number): T | undefined {
+        let currentNode = this.head;
+        if (idx >= this.length) {
+            return;
+        }
+
+        for (let i = 0; i < this.length; i++) {
+            if (idx === i) {
+                break;
+            }
+            currentNode = currentNode?.next;
+        }
+
+        let removeNode = currentNode;
+        currentNode = currentNode?.next;
+
+        return currentNode?.value;
+    }
 }
