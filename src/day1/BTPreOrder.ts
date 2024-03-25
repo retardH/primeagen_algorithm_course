dfe7377c7dc10db801ea56dc31fdff8a28ce6337
HtetZarni <loowmaad@gmail.com>
2024-03-25 23:10:03 +0630

    implemented binary tree search

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    // 1) pre
+    path.push(curr.value);
+
+    // 2) recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // 3) post
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
