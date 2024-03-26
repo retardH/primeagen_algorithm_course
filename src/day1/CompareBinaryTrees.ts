b4c9eb9795354fc3635029dd6f349417850bfc76
HtetZarni <loowmaad@gmail.com>
2024-03-26 14:13:44 +0630

    implement binary tree search

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,21 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // structural check to see if we can or cannot recurse further
+    if (a === null && b === null) {
+        return false;
+    }
 
+    // structural check to compare the shape of the trees
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
